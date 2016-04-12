fc8f974a4c2d493ae51c5e3f86c6dc682fe676c8
Yaroslav Nikonorov <nikonorov.y@gmail.com>
2016-04-12 21:17:18 +0300

    Added static

---
+++ static/js/script.js
@@ -0,0 +1,31 @@
+angular.module('myApp', [])
+	.controller('customersCtrl', ['$scope','$http', function($scope,$http) {
+		$http.get("/EcoForm/group/getAll").
+			then(function(response) {
+				$scope.myData = response.data;
+			}
+		);
+		
+		$scope.filterPeople = function() {								
+			var dataObj = {
+					"skills": $('#dropdownMenu1').text(),
+					"joinUmka": $('#dropdownMenu2').text(),
+					"timeWithUs": $('#dropdownMenu3').text(),
+					"members": $('#dropdownMenu4').text()
+
+			};	
+			var res = $http.post('/EcoForm/group/getAllByParams', dataObj);
+
+			res.success(function(data, status, headers, config) {
+				$scope.myData = data;
+			});
+			res.error(function(data, status, headers, config) {
+				alert( "failure message: " + JSON.stringify({data: data}));
+			});	
+		
+		};	
+		
+		$scope.displayField = function() {
+			alert('sssss');
+		}
+}])
---
+++ static/js/script_members.js
@@ -0,0 +1,29 @@
+angular.module('myApp', [])
+	.controller('customersCtrl', ['$scope','$http', function($scope,$http) {
+		$http.get("/EcoForm/member/getAll").
+			then(function(response) {
+				$scope.myData = response.data;
+			}
+		);
+		
+		$scope.filterPeople = function() {								
+			var dataObj = {
+					"skills": $('#dropdownMenu1').text(),
+					"responsibility": $('#dropdownMenu2').text(),
+					"timeWithUs": $('#dropdownMenu3').text()
+			};	
+			var res = $http.post('/EcoForm/member/getAllByParams', dataObj);
+
+			res.success(function(data, status, headers, config) {
+				$scope.myData = data;
+			});
+			res.error(function(data, status, headers, config) {
+				alert( "failure message: " + JSON.stringify({data: data}));
+			});	
+		
+		};	
+		
+		$scope.displayField = function() {
+			alert('sssss');
+		}
+}])
